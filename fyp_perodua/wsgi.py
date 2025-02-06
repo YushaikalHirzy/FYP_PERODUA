c0394a4c13d12bc452a0bed527c3b8d6cce2fb35
YushaikalHirzy <209961@student.upm.edu.my>
2025-02-06 20:51:04 +0800

    Update wsgi.py

--- fyp_perodua/wsgi.py
+++ fyp_perodua/wsgi.py
@@ -14,6 +14,6 @@ sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fyp_perodua.fyp_perodua.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fyp_perodua.settings')
 
 application = get_wsgi_application()
